0308c935dee7e0e6735e0c1d2ff941d1ca1c4aea
Dario Alaniz <dario_a_066@hotmail.com.ar>
2021-02-27 17:07:35 -0300

    Workspace para trabajar en el ultimo diseño de ethernet con CDMA y BRAM

---
+++ workspace_eth_BRAM_CDMA/test_CDMA/Debug/src/helloworld.d
@@ -0,0 +1,64 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xaxicdma.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xaxicdma_bd.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xstatus.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_types.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_assert.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xaxicdma_hw.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_io.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_printf.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xparameters.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/bspconfig.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/mb_interface.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_exception.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xdebug.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xenv.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xenv_standalone.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xdebug.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_exception.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_cache.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xparameters.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xintc.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xintc_l.h
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xaxicdma.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xaxicdma_bd.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xstatus.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_types.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_assert.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xaxicdma_hw.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_io.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_printf.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xparameters.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/bspconfig.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/mb_interface.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_exception.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xdebug.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xenv.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xenv_standalone.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xdebug.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_exception.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_cache.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xparameters.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xintc.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xintc_l.h:
---
+++ workspace_eth_BRAM_CDMA/test_eth_bram_cdma/Debug/src/platform_mb.d
@@ -0,0 +1,70 @@
+src/platform_mb.o src/platform_mb.o: ../src/platform_mb.c \
+ ../src/platform.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xaxicdma.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xaxicdma_bd.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xstatus.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_types.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_assert.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xaxicdma_hw.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_io.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_printf.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xparameters.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/bspconfig.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/mb_interface.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_exception.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xdebug.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xenv.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xenv_standalone.h \
+ ../src/platform_config.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/mb_interface.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xparameters.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xintc.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xintc_l.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xtmrctr_l.h \
+ D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xaxicdma_hw.h
+
+../src/platform.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xaxicdma.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xaxicdma_bd.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xstatus.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_types.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_assert.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xaxicdma_hw.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_io.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_printf.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xparameters.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/bspconfig.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/mb_interface.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xil_exception.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xdebug.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xenv.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xenv_standalone.h:
+
+../src/platform_config.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/mb_interface.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xparameters.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xintc.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xintc_l.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xtmrctr_l.h:
+
+D:/dario/fulgor/trabajo_final/workspace_ethernet/workspace_eth_BRAM_CDMA/eth1_wrapper/export/eth1_wrapper/sw/eth1_wrapper/standalone_domain/bspinclude/include/xaxicdma_hw.h:
